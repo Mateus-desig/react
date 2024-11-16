bffabb429d6672492656114c06f88272138436a2
Mateus <utracraft900@gmail.com>
2024-11-16 15:27:55 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+function App() {
+
+  const [msg, setMessage] = useState('Olá mundo!')
+
+  return <div>
+    <h1>{msg}</h1>
+    <button onClick={() => {
+      
+      setMessage('Boa noite mundo')
+
+    }}>Mudar Mensagem</button>
+  </div>
+
+}
+
+export default App
