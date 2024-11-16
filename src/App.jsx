80b9bcd865c35d3fee834504ace8d46c608d6d52
Mateus <utracraft900@gmail.com>
2024-11-16 17:56:10 +0000

    estilização

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import { useState } from "react"
+import Tasks from "./components/Task"
+import AddTasks from "./components/AddTasks"
 
 function App() {
 
-  const [msg, setMessage] = useState('Olá mundo!')
+  const [Tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "",
+      description: ""
+    }
 
-  return <div>
-    <h1>{msg}</h1>
-    <button onClick={() => {
-      
-      setMessage('Boa noite mundo') 
+  ])
 
-    }}>Mudar Mensagem</button>
+  return <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+
+    <div className="w-[500px]">
+
+      <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
+
+      <AddTasks />
+      <Tasks />
+
+    </div>
+
   </div>
 
 }
